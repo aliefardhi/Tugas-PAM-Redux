23bd97df0b4f3ba02219e9f30ea6e208fb298519
aliefardhi <alief.118140003@student.itera.ac.id>
2021-03-08 23:05:34 +0700

    upload tugas pam redux

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/Header'
+import Item from './components/Item'
+import ListItem from './components/ListItem'
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Header />
+      <Item />
+      <ListItem/>
     </View>
   );
 }
